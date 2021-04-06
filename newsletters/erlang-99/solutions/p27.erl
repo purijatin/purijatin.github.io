ae9cba47e24e2bb7dbc281c203d1adf786017665
Jatin <jatin@bayestree.com>
2021-04-06 18:17:54 +0530

    erl

--- newsletters/erlang-99/solutions/p27.erl
+++ newsletters/erlang-99/solutions/p27.erl
@@ -1,9 +1,18 @@
 %In how many ways can a group of 9 people work in 3 disjoint subgroups of 2, 3 and 4 persons? Write a function that generates all the possibilities.
 -module(p27).
--export([group/2]).
+-export([group/2, group3/1]).
 
-group([], Ls) ->
-    Ls;
+group([], []) ->
+    [];
+
+group([_ | []], Ls) ->
+    [[Ls]];
 
 group([G | Rest], Ls) ->
-    [ [F | group(Rest, Ls -- F)] || F <- p26:combinations(G, Ls)].
+    F = p26:combinations(G, Ls),
+    lists:flatmap(fun(Comb) -> 
+        E = group(Rest, Ls -- Comb),
+        lists:map(fun(B) -> [Comb] ++ B end, E)
+    end ,F).
+
+group3(Ls) -> group([2,3,4], Ls).
